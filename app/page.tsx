e63b144c9e431bb1fdc9ee63814a999eae03d100
Boris Besemer <borisbesemer@gmail.com>
2025-08-27 21:41:33 +0200

    fix: remove remote components test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Chat } from "@/components/chat";
 import { debugFlag } from "@/lib/flags";
-import { RemoteComponent } from "remote-components/next/host";
 
 export default async function Home() {
 	// TODO: Add precompute path for static chat view
@@ -8,7 +7,6 @@ export default async function Home() {
 
 	return (
 		<div className="h-screen w-full">
-			<RemoteComponent src="/components/bla/product-details" />
 			<Chat isDebug={isDebug} />
 		</div>
 	);
